f9a466f676c4c406068fb8ab2b1d99633eeaa47d
legionOfGo <folayanjoey@gmail.com>
2024-12-26 13:22:28 +0100

    yui

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,16 @@
-# build_files.sh
-pip install -r requirements.txt
+#!/bin/bash
+# Install Python and pip if they are not available
+if ! command -v python3 &> /dev/null
+then
+    echo "Python is not installed. Installing Python..."
+    apt-get update && apt-get install -y python3 python3-pip
+fi
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 is not installed. Installing pip3..."
+    apt-get install -y python3-pip
+fi
+
+# Run pip to install dependencies
+pip3 install -r requirements.txt
